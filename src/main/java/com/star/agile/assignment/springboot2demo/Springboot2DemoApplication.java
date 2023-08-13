d6fd38767b84b6bbf505b73936dd2517c694bfc6
gouthamshetty <31468186+gouthamshetty@users.noreply.github.com>
2023-08-13 18:16:54 +0530

    corrected springboot for docker

---
+++ src/main/java/com/star/agile/assignment/springboot2demo/Springboot2DemoApplication.java
@@ -0,0 +1,13 @@
+package com.star.agile.assignment.springboot2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/star/agile/assignment/springboot2demo/Springboot2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.star.agile.assignment.springboot2demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
